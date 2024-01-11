390cb7a3c9a753f988f1055408fe7cf34ab4db61
Nick Schneble <nschneble@users.noreply.github.com>
2024-01-11 15:30:29 -0500

    let's try this again

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,6 @@ export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
         '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     },
-    base: command === 'serve' ? '' : 'https://nschneble.github.io/currently'
+    base: command === 'serve' ? '' : '/currently'
   }
 })
