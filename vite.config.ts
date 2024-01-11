19556d5c5f42eb0b477ed834b39626df253b5ae1
Nick Schneble <nschneble@users.noreply.github.com>
2024-01-11 15:25:31 -0500

    define Vitest config as a function

--- vite.config.ts
+++ vite.config.ts
@@ -6,33 +6,16 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
-  if (command === 'serve') {
-    // development config
-    return {
-      plugins: [
-        vue(),
-        vueJsx(),
-      ],
-      resolve: {
-        alias: {
-          '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-      },
-      base: ''
-    }
-  } else {
-    // production config
-    return {
-      plugins: [
-        vue(),
-        vueJsx(),
-      ],
-      resolve: {
-        alias: {
-          '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-      },
-      base: 'https://nschneble.github.io/currently'
-    }
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    base: command === 'serve' ? '' : 'https://nschneble.github.io/currently'
   }
 })
